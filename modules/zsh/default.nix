379905c842f8fed66b9d4ac2164f7a1e0ad3c204
Tyler Lafayette <tylerlafayette@gmail.com>
2021-10-29 19:05:15 -0700

    Add `zsh-vim-mode` plugin

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -20,6 +20,15 @@ let
 
     # This is so sad
     eval "$(/opt/homebrew/bin/brew shellenv)"
+
+    # Vim mode
+    MODE_CURSOR_VIINS="#00ff00 blinking bar"
+    MODE_CURSOR_REPLACE="$MODE_CURSOR_VIINS #ff0000"
+    MODE_CURSOR_VICMD="green block"
+    MODE_CURSOR_SEARCH="#ff00ff steady underline"
+    MODE_CURSOR_VISUAL="$MODE_CURSOR_VICMD steady bar"
+    MODE_CURSOR_VLINE="$MODE_CURSOR_VISUAL #00ffff"
+    KEYTIMEOUT=5
   '';
   functions = ''
     # Convenience function to open home.nix.
@@ -64,6 +73,15 @@ in
           file = "fzf-tab.plugin.zsh";
         })
         (mkZshPlugin { pkg = zsh-syntax-highlighting; })
+        {
+          name = "zsh-vim-mode";
+          src = fetchFromGitHub {
+            owner = "softmoth";
+            repo = "zsh-vim-mode";
+            rev = "1f9953b7d6f2f0a8d2cb8e8977baa48278a31eab";
+            sha256 = "1i79rrv22mxpk3i9i1b9ykpx8b4w93z2avck893wvmaqqic89vkb";
+          };
+        }
       ];
 
       initExtra = builtins.concatStringsSep "\n" [zshTheme configs functions];
